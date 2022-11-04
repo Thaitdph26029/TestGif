fad9c611a691b0156a9eecd070fb32f0216bb543
duythai010305@gmail.com <duythai010305@gmail.com>
2022-11-04 21:13:59 +0700

    Add project files.

---
+++ TestGif/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestGif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
